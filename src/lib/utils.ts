b3afa5a14a5fc9c1ecde9a63bb94f8eacda00cf1
Gourav-21 <rahul.dora21@gmail.com>
2024-05-24 22:15:26 +0530

    added generatehtml fun

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,34 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function generateHTML(data) {
+  let html = `<h2>${data.type} - ${data.type_rengjøring}</h2>`;
+
+  for (const property in data) {
+    if (data.hasOwnProperty(property)) {
+      const keyWithoutUnderscore = property.replace(/_/g, '');
+
+      if (Array.isArray(data[property])) {
+        html += `<p><b>${keyWithoutUnderscore}:</b></p>`;
+        html += `<ul>`;
+        for (const item of data[property]) {
+          html += `<li>${item}</li>`;
+        }
+        html += `</ul>`;
+      } else if (typeof data[property] === 'object') {
+        html += `<p><b>${keyWithoutUnderscore}:</b></p>`;
+        for (const subProperty in data[property]) {
+          if (data[property].hasOwnProperty(subProperty)) {
+            const subKeyWithoutUnderscore = subProperty.replace(/_/g, '');
+            html += `<p>&nbsp;&nbsp;<b>${subKeyWithoutUnderscore}:</b> ${data[property][subProperty]}</p>`;
+          }
+        }
+      } else {
+        html += `<p><b>${keyWithoutUnderscore}:</b> ${data[property]}</p>`;
+      }
+    }
+  }
+
+  return html;
+}
